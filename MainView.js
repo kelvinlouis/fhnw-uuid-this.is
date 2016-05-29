4b9892264eb10803ab568d47857ca040ecea35b7
Kelvin Louis <me@kelvinlouis.com>
2016-05-29 15:11:19 +0200

    Created a Toast Message after Login

--- MainView.js
+++ MainView.js
@@ -1,16 +1,33 @@
 var Observable = require('FuseJS/Observable'),
-    activePage = Observable('Login');
+    activePage = Observable('Login'),
+    toastText = Observable(''),
+    toastTrue = Observable(false),
+
+    showToast = function(msg) {
+      toastText.value = msg;
+      toastTrue.value = true;
+
+      // Removes the toast again
+      setTimeout(function() {
+        toastTrue.value = false;
+      }, 2000 );
+    };
 
   module.exports = {
     // Simple Callback-Routing
     activePage: activePage,
     completeLogin: function() {
       activePage.value = 'LoginSetInterests';
+      showToast('Successfully logged in');
     },
     selectedInterests: function() {
       activePage.value = 'Questions';
     },
     createQuestion: function() {
       activePage.value = 'CreateQuestion';
-    }
+    },
+
+    // Toast Observables
+    toastText: toastText,
+    toastTrue: toastTrue
 };
